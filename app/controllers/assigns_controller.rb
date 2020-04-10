9eca0380e012878a2dbf2f9b299a2b38d43ef7e0
root <root@DESKTOP-3IBCSAR.localdomain>
2020-04-10 21:07:18 +0200

    fixed assign member must exist

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -3,12 +3,16 @@ class AssignsController < ApplicationController
 
   def create
     team = Team.friendly.find(params[:team_id])
-    user = email_reliable?(assign_params) ? User.find_or_create_by_email(assign_params) : nil
-    if user
-      team.invite_member(user)
-      redirect_to team_url(team), notice: I18n.t('views.messages.assigned')
+    if User.where(email: assign_params).any?
+      user = email_reliable?(assign_params) ? User.find_or_create_by_email(assign_params) : nil
+      if user
+        team.invite_member(user)
+        redirect_to team_url(team), notice: I18n.t('views.messages.assigned')
+      else
+        redirect_to team_url(team), notice: I18n.t('views.messages.failed_to_assign')
+      end
     else
-      redirect_to team_url(team), notice: I18n.t('views.messages.failed_to_assign')
+      redirect_to team_url(team), notice: I18n.t('views.messages.user_must_exist')
     end
   end
 
@@ -30,15 +34,11 @@ class AssignsController < ApplicationController
       I18n.t('views.messages.cannot_delete_the_leader')
     elsif Assign.where(user_id: assigned_user.id).count == 1
       I18n.t('views.messages.cannot_delete_only_a_member')
-    elsif current_user == assign.team.owner || current_user == assigned_user
-      if assign.destroy
-        set_next_team(assign, assigned_user)
-        I18n.t('views.messages.delete_member')
-      else
-        I18n.t('views.messages.cannot_delete_member_4_some_reason')
-      end
+    elsif assign.destroy
+      set_next_team(assign, assigned_user)
+      I18n.t('views.messages.delete_member')
     else
-      I18n.t('views.messages.not_authorized')
+      I18n.t('views.messages.cannot_delete_member_4_some_reason')
     end
   end
   
